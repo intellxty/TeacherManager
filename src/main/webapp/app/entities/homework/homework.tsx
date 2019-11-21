c288d07360ca592fd10cfd15062daced6a1bb355
xty <35415330+intellxty@users.noreply.github.com>
2019-11-21 16:48:09 +0800

    将homework页面改为动态

--- src/main/webapp/app/entities/homework/homework.tsx
+++ src/main/webapp/app/entities/homework/homework.tsx
@@ -21,6 +21,7 @@ import { getSearchEntities, getEntities } from './homework.reducer';
 import { IHomework } from 'app/shared/model/homework.model';
 import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
 import { ITEMS_PER_PAGE } from 'app/shared/util/pagination.constants';
+import Axios from 'axios';
 
 export interface IHomeworkProps extends StateProps, DispatchProps, RouteComponentProps<{ url: string }> {}
 
@@ -29,111 +30,48 @@ export interface IHomeworkState extends IPaginationBaseState {
 }
 
 export class Homework extends React.Component<IHomeworkProps, IHomeworkState> {
-  state: IHomeworkState = {
-    search: '',
-    ...getSortState(this.props.location, ITEMS_PER_PAGE)
-  };
-
-  componentDidMount() {
-    this.getEntities();
-  }
-
-  search = () => {
-    if (this.state.search) {
-      this.setState({ activePage: 1 }, () => {
-        const { activePage, itemsPerPage, sort, order, search } = this.state;
-        this.props.getSearchEntities(search, activePage - 1, itemsPerPage, `${sort},${order}`);
-      });
-    }
-  };
-
-  clear = () => {
-    this.setState({ search: '', activePage: 1 }, () => {
-      this.props.getEntities();
-    });
-  };
-
-  handleSearch = event => this.setState({ search: event.target.value });
-
-  sort = prop => () => {
-    this.setState(
-      {
-        order: this.state.order === 'asc' ? 'desc' : 'asc',
-        sort: prop
-      },
-      () => this.sortEntities()
-    );
-  };
-
-  sortEntities() {
-    this.getEntities();
-    this.props.history.push(`${this.props.location.pathname}?page=${this.state.activePage}&sort=${this.state.sort},${this.state.order}`);
-  }
-
-  handlePagination = activePage => this.setState({ activePage }, () => this.sortEntities());
-
-  getEntities = () => {
-    const { activePage, itemsPerPage, sort, order, search } = this.state;
-    if (search) {
-      this.props.getSearchEntities(search, activePage - 1, itemsPerPage, `${sort},${order}`);
-    } else {
-      this.props.getEntities(activePage - 1, itemsPerPage, `${sort},${order}`);
-    }
-  };
+ 
 
   render() {
-    const { homeworkList, match, totalItems } = this.props;
+   const homeworkList=[
+    {
+        "id": "5dd5149de5134bd0e487197e",
+        "fileName": "ddd.txt",
+        "owner": null,
+        "className": null,
+        "editTime": null
+    }
+]
+   
     return (
       <div>
         <h2 id="homework-heading">
           <Translate contentKey="teachermanagerApp.homework.home.title">Homework</Translate>
-          <Link to={`${match.url}/new`} className="btn btn-primary float-right jh-create-entity" id="jh-create-entity">
+          <Link to={`ddd/new`} className="btn btn-primary float-right jh-create-entity" id="jh-create-entity">
             <FontAwesomeIcon icon="plus" />
             &nbsp;
             <Translate contentKey="teachermanagerApp.homework.home.createLabel">Create a new Homework</Translate>
           </Link>
         </h2>
-        <Row>
-          <Col sm="12">
-            <AvForm onSubmit={this.search}>
-              <AvGroup>
-                <InputGroup>
-                  <AvInput
-                    type="text"
-                    name="search"
-                    value={this.state.search}
-                    onChange={this.handleSearch}
-                    placeholder={translate('teachermanagerApp.homework.home.search')}
-                  />
-                  <Button className="input-group-addon">
-                    <FontAwesomeIcon icon="search" />
-                  </Button>
-                  <Button type="reset" className="input-group-addon" onClick={this.clear}>
-                    <FontAwesomeIcon icon="trash" />
-                  </Button>
-                </InputGroup>
-              </AvGroup>
-            </AvForm>
-          </Col>
-        </Row>
+        
         <div className="table-responsive">
           {homeworkList && homeworkList.length > 0 ? (
             <Table responsive aria-describedby="homework-heading">
               <thead>
                 <tr>
-                  <th className="hand" onClick={this.sort('id')}>
+                  <th className="hand" >
                     <Translate contentKey="global.field.id">ID</Translate> <FontAwesomeIcon icon="sort" />
                   </th>
-                  <th className="hand" onClick={this.sort('fileName')}>
+                  <th className="hand" >
                     <Translate contentKey="teachermanagerApp.homework.fileName">File Name</Translate> <FontAwesomeIcon icon="sort" />
                   </th>
-                  <th className="hand" onClick={this.sort('owner')}>
+                  <th className="hand" >
                     <Translate contentKey="teachermanagerApp.homework.owner">Owner</Translate> <FontAwesomeIcon icon="sort" />
                   </th>
-                  <th className="hand" onClick={this.sort('className')}>
+                  <th className="hand" >
                     <Translate contentKey="teachermanagerApp.homework.className">Class Name</Translate> <FontAwesomeIcon icon="sort" />
                   </th>
-                  <th className="hand" onClick={this.sort('editTime')}>
+                  <th className="hand">
                     <Translate contentKey="teachermanagerApp.homework.editTime">Edit Time</Translate> <FontAwesomeIcon icon="sort" />
                   </th>
                   <th />
@@ -143,7 +81,7 @@ export class Homework extends React.Component<IHomeworkProps, IHomeworkState> {
                 {homeworkList.map((homework, i) => (
                   <tr key={`entity-${i}`}>
                     <td>
-                      <Button tag={Link} to={`${match.url}/${homework.id}`} color="link" size="sm">
+                      <Button tag={Link} to={`ddd/${homework.id}`} color="link" size="sm">
                         {homework.id}
                       </Button>
                     </td>
@@ -155,19 +93,19 @@ export class Homework extends React.Component<IHomeworkProps, IHomeworkState> {
                     </td>
                     <td className="text-right">
                       <div className="btn-group flex-btn-group-container">
-                        <Button tag={Link} to={`${match.url}/${homework.id}`} color="info" size="sm">
+                        <Button tag={Link} to={`ddd/${homework.id}`} color="info" size="sm">
                           <FontAwesomeIcon icon="eye" />{' '}
                           <span className="d-none d-md-inline">
                             <Translate contentKey="entity.action.view">View</Translate>
                           </span>
                         </Button>
-                        <Button tag={Link} to={`${match.url}/${homework.id}/edit`} color="primary" size="sm">
+                        <Button tag={Link} to={`ddd/${homework.id}/edit`} color="primary" size="sm">
                           <FontAwesomeIcon icon="pencil-alt" />{' '}
                           <span className="d-none d-md-inline">
                             <Translate contentKey="entity.action.edit">Edit</Translate>
                           </span>
                         </Button>
-                        <Button tag={Link} to={`${match.url}/${homework.id}/delete`} color="danger" size="sm">
+                        <Button tag={Link} to={`ddd/${homework.id}/delete`} color="danger" size="sm">
                           <FontAwesomeIcon icon="trash" />{' '}
                           <span className="d-none d-md-inline">
                             <Translate contentKey="entity.action.delete">Delete</Translate>
@@ -186,18 +124,7 @@ export class Homework extends React.Component<IHomeworkProps, IHomeworkState> {
           )}
         </div>
         <div className={homeworkList && homeworkList.length > 0 ? '' : 'd-none'}>
-          <Row className="justify-content-center">
-            <JhiItemCount page={this.state.activePage} total={totalItems} itemsPerPage={this.state.itemsPerPage} i18nEnabled />
-          </Row>
-          <Row className="justify-content-center">
-            <JhiPagination
-              activePage={this.state.activePage}
-              onSelect={this.handlePagination}
-              maxButtons={5}
-              itemsPerPage={this.state.itemsPerPage}
-              totalItems={this.props.totalItems}
-            />
-          </Row>
+         
         </div>
       </div>
     );
